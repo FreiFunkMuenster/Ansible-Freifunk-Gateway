d3a7b7ed7ae1a6c823c169c8eb801f4959ba7a04
Michael Suelmann <suelmann@gmx.de>
2016-02-05 22:44:41 +0100

    Bind: Skript für Zone knoten.freifunk-muensterland.de verbessert: - TTLs herabgesetzt - Variablen umbenannt und Datenstruktur sprechender gemacht - Kommentare eingefügt - Determinismus für gleiche Hostnamen gefixt

--- bind_services/files/create_db_knoten.py
+++ bind_services/files/create_db_knoten.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # Python3-Skript zum Erzeugen des Zonefiles für knoten.freifunk-muesnterland.de
-# Aufruf: create_db_knoten.py nodes.json Beispiel-Zonefile > Ausgabe-Zonefile
+# Aufruf: create_db_knoten.py nodes_processed.json Beispiel-Zonefile > Ausgabe-Zonefile
 
 import sys, re, json, time
 
@@ -9,38 +9,42 @@ import sys, re, json, time
 prefix6 = '2a03:2260:115:'
 
 if len(sys.argv) != 3:
-    print("Aufruf: create_db_knoten.py nodes.json Beispiel-Zonefile > Ausgabe", file=sys.stderr)
+    print("Aufruf: create_db_knoten.py nodes_processed.json Beispiel-Zonefile > Ausgabe", file=sys.stderr)
     sys.exit(2)
 file_nodes = sys.argv[1]
 file_db = sys.argv[2]
 
-nodes = {}
-jnodes = json.load(open(file_nodes, 'r'))
-for node in jnodes['nodes'].values():
+nodes_processed = {}
+nodes_json = json.load(open(file_nodes, 'r'))
+for node in nodes_json['nodes'].values():
     try:
         hostname = re.sub(r'[^a-z0-9\-]',"", node["nodeinfo"]["hostname"].lower())[0:63]
         for address in node["nodeinfo"]["network"]["addresses"]:
             if address.lower().startswith(prefix6):
                 online = node['flags']['online']
                 lastseen = node['lastseen']
-                if hostname in nodes:
-                    if not online and nodes[hostname][1]:
+                if hostname in nodes_processed:
+# hostname already exists. Let the node win which fits better:
+                    if not online and nodes_processed[hostname]['online']:
+# online beats offline
                         continue
-                    if not online and nodes[hostname][2] > lastseen:
+                    if not online and nodes_processed[hostname]['lastseen'] > lastseen:
+# bigger lastseen wins if both are offline
                         continue
-                    if (online == nodes[hostname][1]) and nodes[hostname][0] > address:
+                    if (online or nodes_processed[hostname]['lastseen'] > lastseen) and online == nodes_processed[hostname]['online'] and nodes_processed[hostname]['address'] > address:
+# otherwise bigger IP address wins
                         continue
-                nodes[hostname] = (address, online, lastseen)
+                nodes_processed[hostname] = {'address': address, 'online': online, 'lastseen': lastseen}
     except:
         pass
 
 print(
 '''; zonefile for knoten.freifunk-muensterland.de.
-$TTL    86400
+$TTL    3600
 @       IN      SOA     service.freifunk-muensterland.de. info.freifunk-muensterland.de. (
                      {}         ; Serial
-                         604800         ; Refresh
-                          86400         ; Retry
+                          86400         ; Refresh
+                           7200         ; Retry
                         2419200         ; Expire
                           86400 )       ; Negative Cache TTL
 '''.format(time.strftime("%s")), end=""
@@ -48,5 +52,5 @@ $TTL    86400
 for line in open(file_db, 'r'):
     if re.search('IN\s*NS', line):
         print(line, end="")
-for hostname in sorted(nodes):
-    print(hostname + "  AAAA    " + nodes[hostname][0])
+for hostname in sorted(nodes_processed):
+    print(hostname + "  AAAA  " + nodes_processed[hostname]['address'])
