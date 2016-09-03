0b650cfd0954c89bef7a8bdaded1f633484cab30
Matthias P. Walther <matthiaspeterw@aol.com>
2016-09-03 12:29:48 +0200

    - Sperrliste für Knoten-IDs, sodass diese sich nicht verbinden können

--- gateways_l2tp/files/addif.sh
+++ gateways_l2tp/files/addif.sh
@@ -2,5 +2,11 @@
 INTERFACE="$3"
 PORT="$9"
 DOM=${PORT#200}
-ip link set dev $INTERFACE up mtu 1364
-brctl addif br$DOM $INTERFACE
+if grep -Fq "$8" "/srv/tunneldigger/broker/scripts/sperrliste.txt"
+then
+	echo "Der Knoten $8 ist auf der Sperrliste, verweigere Zugang."
+	exit 1
+else
+	ip link set dev $INTERFACE up mtu 1364
+	brctl addif br$DOM $INTERFACE
+fi
